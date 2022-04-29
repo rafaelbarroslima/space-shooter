d1e6809163fd4a0e9d7540eb4b38250fbc4c349e
rafaelbarroslima <rafaelbarroslimaa@gmail.com>
2022-04-29 00:59:34 -0300

    update

---
+++ script.js
@@ -0,0 +1,72 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-area');
+
+// Movimento e tiro da nave
+function flyShip(event) {
+    if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        moveUp();
+    } else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    } else if (event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+}
+
+// Função de subir
+function moveUp() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if (topPosition === '0px') {
+        return;
+    } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+// Função de descer
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if (topPosition === '500px') {
+        return;
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+// Funcionalidade de tiro
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = './images/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
+
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+
+        if (xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`;
+        }
+    }, 10);
+}
+
+window.addEventListener('keydown', flyShip);
